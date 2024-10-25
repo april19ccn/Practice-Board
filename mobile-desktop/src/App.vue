fb94a760eb67d9f87c99775b38d2ee887e409215
April <april1998@88.com>
2024-10-25 17:34:09 +0800

    feat: 动画效果

---
+++ mobile-desktop/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DownLoad from './components/DownLoad.vue';
+
+const resizeHandler = () => {
+  const rect = document.body.getBoundingClientRect()
+  const isMobile = rect.width < 980
+  appStore.setDevice(isMobile ? 'mobile' : 'desktop')
+  appStore.setCollapse(isMobile)
+}
+
+</script>
+
+<template>
+  <main class="main-content">
+    <DownLoad />
+  </main>
+</template>
+
+<style scoped>
+.main-content {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+</style>

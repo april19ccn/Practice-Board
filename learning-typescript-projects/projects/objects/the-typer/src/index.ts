4b0dca30d488bd1dce726659b27ba73897788c3a
April <april1998@88.com>
2025-04-17 13:16:32 +0800

    feat: lts-up

--- learning-typescript-projects/projects/objects/the-typer/src/index.ts
+++ learning-typescript-projects/projects/objects/the-typer/src/index.ts
@@ -1,128 +1,170 @@
 // Write your types here! ✨
+type Base = {
+    name: string,
+    proximity: number,
+    treasure?: string,
+}
 
-let current = {
-	name: "Woesong Bridge",
-	proximity: 100,
-	through: {
-		area: "middle",
-		downstream: {
-			around: {
-				area: "end",
-				upstream: {
-					name: "Vizima",
-					proximity: 30,
-					type: "clearing",
-				},
-				name: "White Orchard Creek",
-				proximity: 25,
-				type: "stream",
-			},
-			name: "Oxenfurt Gate",
-			proximity: 40,
-			through: {
-				name: "Vergen Tunnel",
-				proximity: 20,
-				shortcut: {
-					proximity: 30,
-					name: "Crow's Perch",
-					type: "town",
-				},
-				through: {
-					area: "begin",
-					downstream: {
-						through: {
-							treasure: "rare playing cards",
-							name: "Reuven's Treasure",
-							proximity: 0,
-							type: "clearing",
-						},
-						name: "Gate of the Hierarch",
-						proximity: 10,
-						type: "town",
-					},
-					name: "Founders Stream",
-					proximity: 25,
-					type: "stream",
-				},
-				type: "path",
-			},
-			type: "town",
-		},
-		name: "Yavina River",
-		proximity: 50,
-		type: "stream",
-		upstream: {
-			name: "Merchants' Trail",
-			proximity: 65,
-			through: {
-				name: "Beauclair",
-				proximity: 70,
-				type: "town",
-			},
-			type: "path",
-		},
-	},
-	type: "path",
+type Path = Base & {
+    shortcut?: Info,
+    through: Info,
+    type: "path"
+}
+
+type Town = Base & {
+    around?: Info,
+    through?: Info,
+    type: "town"
+}
+
+type PathClearing = Base & {
+    through?: Info
+    type: "clearing"
+}
+
+type StreamBegin = {
+    area: "begin",
+    downstream: Info
+}
+
+type StreamMiddle = {
+    area: "middle",
+    downstream: Info,
+    upstream: Info
+}
+
+type StreamEnd = {
+    area: "end",
+    upstream: Info
+}
+
+type Stream = Base & (StreamBegin | StreamMiddle | StreamEnd) & { type: "stream" }
+
+type Info = Path | Stream | Town | PathClearing
+
+let current: Info | undefined = {
+    name: "Woesong Bridge",
+    proximity: 100,
+    through: {
+        area: "middle",
+        downstream: {
+            around: {
+                area: "end",
+                upstream: {
+                    name: "Vizima",
+                    proximity: 30,
+                    type: "clearing",
+                },
+                name: "White Orchard Creek",
+                proximity: 25,
+                type: "stream",
+            },
+            name: "Oxenfurt Gate",
+            proximity: 40,
+            through: {
+                name: "Vergen Tunnel",
+                proximity: 20,
+                shortcut: {
+                    proximity: 30,
+                    name: "Crow's Perch",
+                    type: "town",
+                },
+                through: {
+                    area: "begin",
+                    downstream: {
+                        through: {
+                            treasure: "rare playing cards",
+                            name: "Reuven's Treasure",
+                            proximity: 0,
+                            type: "clearing",
+                        },
+                        name: "Gate of the Hierarch",
+                        proximity: 10,
+                        type: "town",
+                    },
+                    name: "Founders Stream",
+                    proximity: 25,
+                    type: "stream",
+                },
+                type: "path",
+            },
+            type: "town",
+        },
+        name: "Yavina River",
+        proximity: 50,
+        type: "stream",
+        upstream: {
+            name: "Merchants' Trail",
+            proximity: 65,
+            through: {
+                name: "Beauclair",
+                proximity: 70,
+                type: "town",
+            },
+            type: "path",
+        },
+    },
+    type: "path",
 };
 
 let treasure;
 
 while (current) {
-	console.log(`At: ${current.name}`);
+    console.log(`At: ${current.name}`);
 
-	switch (current.type) {
-		case "clearing":
-			current = current.through;
-			break;
+    switch (current.type) {
+        case "clearing":
+            current = current.through;
+            break;
 
-		case "path":
-			current =
-				current.shortcut &&
-				current.shortcut.proximity < current.through.proximity
-					? current.shortcut
-					: current.through;
-			break;
+        case "path":
+            current =
+                current.shortcut &&
+                    current.shortcut.proximity < current.through.proximity
+                    ? current.shortcut
+                    : current.through;
+            break;
 
-		case "town":
-			if (!current.around) {
-				current = current.through;
-			} else if (!current.through) {
-				current = current.around;
-			} else {
-				current =
-					current.around.proximity < current.through.proximity
-						? current.around
-						: current.through;
-			}
-			break;
+        case "town":
+            if (!current.around) {
+                current = current.through;
+            } else if (!current.through) {
+                current = current.around;
+            } else {
+                current =
+                    current.around.proximity < current.through.proximity
+                        ? current.around
+                        : current.through;
+            }
+            break;
 
-		case "stream":
-			switch (current.area) {
-				case "begin":
-					current = current.downstream;
-					break;
-				case "end":
-					current = current.upstream;
-					break;
-				case "middle":
-					current =
-						current.downstream.proximity < current.upstream.proximity
-							? current.downstream
-							: current.upstream;
-					break;
-			}
-	}
+        case "stream":
+            switch (current.area) {
+                case "begin":
+                    current = current.downstream;
+                    break;
+                case "end":
+                    current = current.upstream;
+                    break;
+                case "middle":
+                    current =
+                        current.downstream.proximity < current.upstream.proximity
+                            ? current.downstream
+                            : current.upstream;
+                    break;
+            }
+    }
 
-	if (!current) {
-		console.log("Hmm. Dead end.");
-	} else if (current.treasure) {
-		treasure = current.treasure;
-		break;
-	}
+    if (!current) {
+        console.log("Hmm. Dead end.");
+    } else if (current.treasure) {
+        treasure = current.treasure;
+        break;
+    }
 }
 
 if (treasure) {
-	console.log(`This will do nicely: ${treasure}.`);
+    console.log(`This will do nicely: ${treasure}.`);
 } else {
-	console.log("Nothing going.");
+    console.log("Nothing going.");
 }

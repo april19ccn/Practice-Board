1356fed6ab6e0bf8efa7191894e344b3c28f194f
April <april1998@88.com>
2025-05-12 10:53:33 +0800

    feat: fp-6-test

--- web-ts/learning-typescript-projects/projects/arrays/text-processor/src/index.ts
+++ web-ts/learning-typescript-projects/projects/arrays/text-processor/src/index.ts
@@ -1,11 +1,65 @@
 // Write your alignTexts function here! ✨
 // You'll need to export it so the tests can run it.
 
-type Option = {
-    align?: "left" | "middle" | "right";
-    width: number;
+export type AlignmentOptions = {
+	align?: "left" | "middle" | "right";
+	width: number;
+};
+
+function handleSplitLine(section: string, width: number): string[] {
+    const strArray = section.split(" ");
+
+    const result: string[] = []
+    while (strArray.length > 0) {
+        let str = strArray.shift() as string
+
+        while (strArray.length > 0 && str.length < width) {
+            const tempStr = str + " " + strArray[0]
+            if (tempStr.length > width) {
+                break
+            } else {
+                strArray.shift()
+                str = tempStr
+            }
+        }
+
+        result.push(str)
+    }
+
+    return result
 }
 
-export function alignTexts(text: string, options: Option): string[][] {
+function handleAlignLine(split: string[], width: number, align: "left" | "middle" | "right" = "left"): string[] {
+    const result = split.map(function (line: string) {
+        const count = width - line.length < 0 ? 0 : width - line.length
+        if (align === "left") {
+            return line + " ".repeat(count)
+        }
+        if (align === "right") {
+            return " ".repeat(count) + line
+        }
+        if (align === "middle") {
+            return " ".repeat(Math.floor((count) / 2)) + line + " ".repeat(Math.ceil((count) / 2))
+        }
+    })
 
+    return result as string[]
 }
+
+export function alignTexts(text: string[], options: AlignmentOptions): string[][] {
+    const formatText = text.map(function (section: string) {
+        return handleAlignLine(handleSplitLine(section, options.width), options.width, options.align)
+    })
+    return formatText
+}
+
+
+
+console.log(alignTexts(["ab cd", "abc def", "a bc def"], { align: "right", width: 4 }))
+
+// Output:
+// [
+// 	["  ab", "  cd"],
+// 	[" abc", " def"],
+// 	["a bc", " def"]
+// ]

7bd529bf81769cadfb485301bc5e405acece4130
April <april1998@88.com>
2025-05-27 18:17:29 +0800

    feat: 3.1-3.2

--- go-learn/ch3/01-integer/integer.go
+++ go-learn/ch3/01-integer/integer.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func bit() {
 	var z = 1 << 1
 	fmt.Printf("%08b\n", z)
 
@@ -27,7 +27,9 @@ func main() {
 
 	fmt.Printf("%08b\n", x<<1) // "01000100", the set {2, 6}
 	fmt.Printf("%08b\n", x>>1) // "00010001", the set {0, 4}
+}
 
+func float() {
 	f := 3.141 // a float64
 	i := int(f)
 	fmt.Println(f, i) // "3.141 3"
@@ -41,4 +43,38 @@ func main() {
 
 	f2 := 1e100
 	fmt.Println(int(f2))
+}
+
+func binary() {
+	// fmt 的技巧
+	// 1. %之后的[1]副词告诉Printf函数再次使用第一个操作数
+	// 2. %后的#副词告诉Printf在用%o、%x或%X输出时生成0、0x或0X前缀
+	// %#[1]o #在[1]前， 不能写成 %[1]#o
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o \n", o) // "438 666 0666"
+	x1 := int64(0xdeadbeef)
+	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x1)
+	// Output:
+	// 3735928559 deadbeef 0xdeadbeef 0XDEADBEEF
+}
+
+// 字符面值通过一对单引号直接包含对应字符
+// 可以通过转义的数值来表示任意的Unicode码点对应的字符
+func char() {
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 国 '国'"
+	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
+}
+
+func main() {
+	bit()
+
+	float()
+
+	binary()
+
+	char()
 }
---
+++ go-learn/ch3/02 -float/01-float/float.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// 常量表达式比较
+	fmt.Println(0.1+0.2 == 0.3) // 输出：true（高精度计算）
+
+	// 变量运算比较
+	a, b := 0.1, 0.2
+	fmt.Println(a+b == 0.3) // 输出：false（双精度浮点误差）
+
+	var f float32 = 16777216 // 1 << 24
+	fmt.Println(f)
+	fmt.Println(f + 1)
+	fmt.Println(f == f+1) // "true"!
+
+	// 验证无穷大是否唯一
+	var z float64
+	fmt.Println(math.Inf(1) == 1/z)
+	fmt.Println(math.Inf(1) == math.Inf(1))  // true（同符号无穷相等）
+	fmt.Println(math.Inf(1) == math.Inf(-1)) // false（符号不同）
+}

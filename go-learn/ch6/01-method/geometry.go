4ec656f438235ef3cce072f35b4dc7f46b49cfa4
April <april1998@88.com>
2025-07-16 16:22:35 +0800

    feat: 6.1-6.2

---
+++ go-learn/ch6/01-method/geometry.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// A Path is a journey connecting the points with straight lines.
+type Path []Point
+
+// Distance returns the distance traveled along the path.
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+func main() {
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance()) // "12"
+}

5d2c14ba87503986fcf8c1d5c519aa2fcaa03cb8
April <april1998@88.com>
2025-09-04 18:05:29 +0800

    feat: 8.10-un-ex-4

--- go-learn/ch8/10-chat/ex-chat/client/client.go
+++ go-learn/ch8/10-chat/ex-chat/client/client.go
@@ -7,25 +7,51 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
 
+var isAccept = flag.Bool("a", false, "是否接收服务器数据")
+
 // !+
 func main() {
+	flag.Parse()
+
 	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	done := make(chan struct{})
 	go func() {
-		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
-		log.Fatal("done")
+		if *isAccept {
+			io.Copy(os.Stdout, conn) // 客户端收数据
+			log.Fatal("done")
+		} else {
+			reader := bufio.NewReader(conn)
+			for i := 0; i < 5; i++ {
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					break
+				}
+				fmt.Print(line)
+			}
+
+			// 模拟阻塞
+			select {}
+
+			// conn.(*net.TCPConn).CloseRead() // 服务器的 bufio.NewScanner(conn) 会认为是异常连接而结束读取
+		}
 		done <- struct{}{} // signal the main goroutine
 	}()
-	mustCopy(conn, os.Stdin)
+
+	mustCopy(conn, os.Stdin) // 客户端写数据
+
 	conn.Close()
 	<-done // wait for background goroutine to finish
 }

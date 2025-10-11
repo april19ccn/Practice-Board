1a3c5cfdec4d6cef317cd1b7555897ca23b12f59
April <april1998@88.com>
2025-10-11 18:02:23 +0800

    feat: fiber-up

--- go-fiber/Learn-Doc/02-api-fiber/main.go
+++ go-fiber/Learn-Doc/02-api-fiber/main.go
@@ -4,11 +4,15 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New(fiber.Config{
-		// Prefork: true,
+		Prefork: true,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return fiber.NewError(782, "Custom error message")
+	})
+
+	app.Get("/404", func(c *fiber.Ctx) error {
+		return fiber.NewError(404, "no!!")
 	})
 
 	app.Listen(":4000")
